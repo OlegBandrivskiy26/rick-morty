f70ed5e39a9a93321b7b96787d6e12fe1571217d
OlegBandrivskiy26 <bandroleg26@gmail.com>
2024-05-29 16:04:48 +0300

    characters page fix

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, TextField, Box, Button, FormControl, InputLabel, Select, OutlinedInput, MenuItem } from "@mui/material";
+import { Container, TextField, Box, Button, FormControl, InputLabel, Select, OutlinedInput, MenuItem, Grid } from "@mui/material";
 import CastomCard from './CastomCard';
 
 const Characters = () => {
@@ -17,7 +17,6 @@ const Characters = () => {
         setQuery('');
     }
 
-
     useEffect(() => {
         const fetchCharacters = async () => {
             try {
@@ -29,7 +28,6 @@ const Characters = () => {
                         gender: gender
                     }
                 });
-                console.log(response.data);
                 setCharacters(response.data.results);
                 setPageQty(response.data.info.pages);
             } catch (error) {
@@ -43,53 +41,61 @@ const Characters = () => {
     }, [query, page, status, gender]);
 
     return (
-        <Container>
-            <TextField
-                fullWidth
-                label="Search by name"
-                value={query}
-                onChange={(event) => {
-                    setQuery(event.target.value);
-                    setPage(1);
-                }}
-                margin="normal"
-                sx={{ my: 2 }}
-            />
-            <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
-                <FormControl sx={{ my: 2, mr: 2, minWidth: 120 }}>
-                    <InputLabel htmlFor="status-select">Status</InputLabel>
-                    <Select
-                        native
-                        value={status}
-                        onChange={(event) => setStatus(event.target.value)}
-                        input={<OutlinedInput label="Status" id="status-select" />}
-                    >
-                        <option aria-label="None" value="" />
-                        <option value="Alive">Alive</option>
-                        <option value="Dead">Dead</option>
-                        <option value="unknown">Unknown</option>
-                    </Select>
-                </FormControl>
-                <FormControl sx={{ my: 2, minWidth: 120 }}>
-                    <InputLabel id="gender-select-label">Gender</InputLabel>
-                    <Select
-                        labelId="gender-select-label"
-                        id="gender-select"
-                        value={gender}
-                        onChange={(event) => setGender(event.target.value)}
-                        input={<OutlinedInput label="Gender" />}
-                    >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        <MenuItem value="Male">Male</MenuItem>
-                        <MenuItem value="Female">Female</MenuItem>
-                        <MenuItem value="unknown">Unknown</MenuItem>
-                    </Select>
-                </FormControl>
-            </Box>
-            <Button onClick={handleReset} variant="outlined" sx={{ width: 200, height: 50, my: 2 }}>Reset</Button>
-            <CastomCard items={characters} pageQty={pageQty} page={page} setPage={setPage} type="character"/>
+        <Container sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'row' }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={3}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', mt:3}}>
+                        <FormControl sx={{ minWidth: 120, mb: 2 }}>
+                            <InputLabel htmlFor="status-select">Status</InputLabel>
+                            <Select
+                                native
+                                value={status}
+                                onChange={(event) => setStatus(event.target.value)}
+                                input={<OutlinedInput label="Status" id="status-select" />}
+                            >
+                                <option aria-label="None" value="" />
+                                <option value="Alive">Alive</option>
+                                <option value="Dead">Dead</option>
+                                <option value="unknown">Unknown</option>
+                            </Select>
+                        </FormControl>
+                        <FormControl sx={{ minWidth: 120, mb: 2 }}>
+                            <InputLabel id="gender-select-label">Gender</InputLabel>
+                            <Select
+                                labelId="gender-select-label"
+                                id="gender-select"
+                                value={gender}
+                                onChange={(event) => setGender(event.target.value)}
+                                input={<OutlinedInput label="Gender" />}
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value="Male">Male</MenuItem>
+                                <MenuItem value="Female">Female</MenuItem>
+                                <MenuItem value="unknown">Unknown</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Button onClick={handleReset} variant="outlined">Reset</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={9}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+                        <TextField
+                            fullWidth
+                            label="Search by name"
+                            value={query}
+                            onChange={(event) => {
+                                setQuery(event.target.value);
+                                setPage(1);
+                            }}
+                            margin="normal"
+                            sx={{ mb: 2 }}
+                        />
+                        <CastomCard items={characters} pageQty={pageQty} page={page} setPage={setPage} type="character" />
+                    </Box>
+                </Grid>
+            </Grid>
         </Container>
     );
 };
